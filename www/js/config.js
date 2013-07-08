b155ad6e000d3f4b9160a414d04134da1b3fd8a0
JoeNewton <joe.newton@reurgency.com>
2013-07-08 14:01:24 -0400

    Added Custom environment info for Production

--- www/js/config.js
+++ www/js/config.js
@@ -2,7 +2,7 @@
  * Environment variable for the remote server. This is used to select the appropriate remote URL
  * @type {string} Environment Name
  */
-const RemoteServerEnvironment = "Dev";
+const RemoteServerEnvironment = "Prod";
 
 /**
  * Remote URL Variables. One for each possible RemoteServerEnvironment value. Must Be named like
